790c671963c8ae5848023e95f00f957e91f1250b
Daniel <danielfranco2011@gmail.com>
2024-05-18 11:50:22 -0700

    Add comments for recording

--- LogoGen.cpp
+++ LogoGen.cpp
@@ -7,45 +7,61 @@
 * The logo to be generated
 * whether the file should be a sh or txt
 */
+//No need to do so many try-catch blocks. Use blocks only when unexpected occurs (i.e communicating with a 3rd party
+// or something neither you nor the user have control over (other device/program, multi-threading, internet based comms).
+//Handle the event, i.e reset states, release resources (should be using RAII anyways) so that program doesn't crash/have unexpected side-effect.
+//https://youtu.be/Oy-VTqz1_58?t=1553 for a good example
 
+//To see just how expensive try-catch is read https://en.cppreference.com/w/cpp/language/throw
+
+//try-catch is expensive when actual error occurs compared to if-else statements 
+
+
+//Since this is a command line program i'll keep as is. :)
 namespace LogoGen{
     void GenerateLogoFile(int argc, char* argv[])
     {
+    //TODO: 
         try{
             if(std::strcmp(argv[1], "txt") != 0 && std::strcmp(argv[1], "sh") != 0){
                 throw(std::runtime_error("incorrect file type: Please type txt or sh"));
             }
+            
         }
         catch(const std::runtime_error& er){
             std::cerr << er.what() << std::endl;
-            throw;
+            std::exit(-1);
         }
-        if(!std::strcmp(argv[1], "txt"))
-        {
-            std::cout << "creating file" << std::endl;
-            std::ostringstream fileName;
-            fileName <<  argv[0] << "." << argv[1];
-            std::ofstream file(fileName.str());
-            file << "Hello World";
-            file.close();
-        }
-        else if(!std::strcmp(argv[1], "sh"))
-        {
-            std::cout << "creating file" << std::endl;
-            std::ostringstream fileName;
-            fileName <<  argv[0] << "." << argv[1];
-            std::ofstream file(fileName.str());
-            file << "#! /bin/bash\n\n";
-            file << "echo \"Hello World\"";
-            file.close();
 
+        std::cout << "creating file" << std::endl;
+        std::ostringstream fileName;
+        fileName << argv[0] << "." << argv[1];
+        try {
+            std::ofstream file(fileName.str());
+            if (!std::strcmp(argv[1], "txt"))
+            {
+                file << "Hello World";
+            }
+            else if (!std::strcmp(argv[1], "sh"))
+            {  
+                file << "#! /bin/bash\n\n";
+                file << "echo \"Hello World\"";
+            }
+            file.close();
+        }
+        //files dont throw exceptions, instead they use flag bits and we check ourself whether raised or not
+        catch (...)
+        {
+            std::exit(-1);
         }
         return;
     }
 }
 
+//Takes in 3 args: 1)name of the exe 2) Logo to write 3) the type of file (md or sh)
 int main(int argc, char* argv[])
 {
+//TODO: remove try block. check with an if statement and return 2/-1 otherwise.
     try{
         if(argc != 3)
         {
@@ -56,7 +72,7 @@ int main(int argc, char* argv[])
     }
     catch(const std::runtime_error& er){
         std::cerr << er.what() << std::endl;
-        throw;
+        return -1;
     }
     //Call file generator with program argument stripped
     LogoGen::GenerateLogoFile(--argc, ++argv);
